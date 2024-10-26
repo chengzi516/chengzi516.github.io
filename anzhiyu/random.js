9a45d2214b6277e2a6d6d6bc979061e01b2ef307
chengzi516 <p772307283@outlook.com>
2024-10-26 13:55:17 +0800

    Site updated: 2024-10-26 13:55:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/26/hello-world/"];function toRandomPost(){
+var posts=["2024/10/26/cpp-Function overloading/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
