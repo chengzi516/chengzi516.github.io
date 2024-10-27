8d4067e131af90753b1f963ff0b08faf429a5135
chengzi516 <p772307283@outlook.com>
2024-10-27 21:39:42 +0800

    Site updated: 2024-10-27 21:39:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/26/cpp-Function overloading/"];function toRandomPost(){
+var posts=["2024/10/27/1/","2024/10/26/cpp-Function overloading/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
