4a1440075eeb34009438e31277a8b6c1bf108030
chengzi516 <p772307283@outlook.com>
2024-11-03 18:15:59 +0800

    Site updated: 2024-11-03 18:15:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/26/cpp-Function overloading/","2024/11/03/cpp-reference/","2024/11/03/post/"];function toRandomPost(){
+var posts=["2024/10/26/cpp-Function overloading/","2024/11/03/cpp-reference/","2024/11/03/cpp-Member functions/","2024/11/03/cpp-Structure alignment/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
