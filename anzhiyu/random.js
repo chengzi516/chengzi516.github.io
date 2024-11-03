67bd02e4f6911675d9acda9ed27a45e17978d116
chengzi516 <p772307283@outlook.com>
2024-11-03 10:07:55 +0800

    Site updated: 2024-11-03 10:07:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/27/1/","2024/10/26/cpp-Function overloading/"];function toRandomPost(){
+var posts=["2024/10/27/1/","2024/10/26/cpp-Function overloading/","2024/11/03/cpp-reference/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
