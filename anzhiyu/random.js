81e23b1dc9989ed97eb4f4a3a89bcc582cccfb03
chengzi516 <p772307283@outlook.com>
2024-11-06 16:03:48 +0800

    Site updated: 2024-11-06 16:03:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/26/cpp-Function overloading/","2024/11/03/cpp-reference/","2024/11/03/cpp-Member functions/","2024/11/03/cpp-Structure alignment/","2024/11/03/cpp-initlist/","2024/11/03/cpp-Inheritance and polymorphism/","2024/11/03/cpp-Memory management/","2024/11/04/cpp-String implement/"];function toRandomPost(){
+var posts=["2024/10/26/cpp-Function overloading/","2024/11/03/cpp-reference/","2024/11/03/cpp-Member functions/","2024/11/03/cpp-Structure alignment/","2024/11/03/cpp-initlist/","2024/11/03/cpp-Inheritance and polymorphism/","2024/11/03/cpp-Memory management/","2024/11/04/cpp-String implement/","2024/11/06/Life may exist on Jupiter/","2024/11/06/碎碎念/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
